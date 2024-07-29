e19ebb09b9e49db9926b1d8d9be9efc97a4a75da
dishajaingithu <dishabardiya25@gmail.com>
2024-07-29 07:58:54 +0530

    Polymorphism

---
+++ Polymorphism/Program1.java
@@ -0,0 +1,21 @@
+/*
+	============= Java Language =============
+
+	Polymorphism :
+
+ 	* Polymorphism is derived from two Greek Words,"Poly" and "morph",which means 'many' and 'forms'
+	  respectively.Hence,Polymorphism meaning in Java refers to the ability of Objects to take
+	  on many Forms.
+	* Types of Polymorphism :
+
+	  1)Compile Time Binding (Method Overloading).
+	  	* The Methods with the same name can have different numbers of parameters,different
+		  types of Parameters or different orders of Parameters in Method.
+
+	  2)Run Time Binding(Method Overriding).
+	  	* It is a type of runtime polymorphism where the method in the subclass overrides
+		  the method in the superclass,which has the same "name,parameters and Return Type"
+		* Here the method to be called is based on the instance of the subclass,which is assign
+		  to a reference variable of the superclass.
+		Note : Compulsory Required the Parent-Child Relation.
+*/
