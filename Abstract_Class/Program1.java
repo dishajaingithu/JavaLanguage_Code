acf0e0e7b6137a463c5fa7cdfb85341125f532f1
dishajaingithu <dishabardiya25@gmail.com>
2024-07-31 13:10:12 +0530

    abstract_class

---
+++ Abstract_Class/Program1.java
@@ -0,0 +1,22 @@
+/*
+	============= Java Language =============
+
+	Abstract Class :
+
+	* Abstract class is a class that is declared 'abstract'.
+	* It may or may not include abstract methods.
+	* Abstract class cannot be instantiated.
+	* Abstract method can only be used in an abstract class, and it does not have a body.
+	* The Method body is provided by subclass(inherited from).
+	* Method Declaration only in class.
+	* We can't create Oject of Abstract class because Compiler Consider our class is incomplete.
+	* Contructor is Present the abstract class.
+	
+	SYNTAX :
+	abstract class XYZ{
+
+		void abc();
+	}
+*/
+
+
